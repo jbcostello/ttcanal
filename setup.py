c78b2440fd6e0e9fbefd85b4c78e4a9417ab0892
Joe Costello <jbcostello@ucsb.edu>
2018-12-20 11:07:48 -0800

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ttcanal',
+      #version='1.0',
+      #url = 'https://github.com/jbcostello/ttcanal',
+      #author = 'Joe Costello',
+      #author_email = 'jbcostello@physics.ucsb.edu',
+      packages = [],
+      install_requires = [],
+      #entry_points={'console_scripts': ['runPEPS = PEPS.command_line:main']},
+      include_package_data = True,
+      zip_safe = False)
