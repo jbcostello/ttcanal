5c0378d95bf51de045444d9651403d49ac5b2223
Courtney McIntosh <camcintoshpe@yahoo.com>
2019-05-29 22:05:32 -0700

    Comments and Additions
    Added better commenting to previously added/modified code, also added functions for plotting time-animated slices of data along with its fourier transform and a function that reduces noise by averaging a variable number of adjacent frames. More changes to follow.

--- ttcanal/ttcanal.py
+++ ttcanal/ttcanal.py
@@ -8,8 +8,9 @@ import matplotlib.pylab as plt
 #from hsganalysis import newhsganalysis
 #import scipy as sp 
 import matplotlib.animation as animation
-from scipy import ndimage
+#from scipy import ndimage
 import scipy.ndimage.filters as filters
+from scipy.optimize import curve_fit
 
 class ttc(object):
     '''
@@ -19,22 +20,26 @@ class ttc(object):
     '''
 
     def __init__(self, inpt, leakfix = False):
-        # Turns raw data file into array of unsized frames, and creates an attribute for number of frames in a given frame.
-        # 
-        # file: name of raw data file from camera (Str)
-        # 
+        # Turns raw data file or array-like object into
+        #   an array of unsized frames, an array of 64x80 frames, and an attribute for number of frames
+        #   Also includes an attribute that notes whether the current leak has been accounted for
+        #
+        # inpt: name of raw data file from camera or array
+        #
         # Has attributes:
         # self.dat: raw data in np array, each element is a 5120 long frame
         # self.frames: number of frames in the data file
         # self.shapedat: the raw data shaped into  a list of 64x80 2d np arrays. Each element of the list is a shaped frame of the data 
+        #
+        # except statements allow code to run without errors if improper data is given
         
         if type(inpt) == str:
             try:
-                raw = np.genfromtxt(file, skip_footer = 2)
+                raw = np.genfromtxt(inpt, skip_footer = 2)
                 self.dat = raw[:,1:5121]
                 self.frames = len(self.dat)
                 try:    
-                    self.shapedat = raw.reshape((-1, 64, 80))
+                    self.shapedat = self.dat.reshape((-1, 64, 80))
                 except:
                     print("Data from file cannot be shaped into frames.")
                     return False
@@ -110,7 +115,6 @@ class ttc(object):
         # Verbose: Provides additional info for debugging purposes, defaults false (Boolean)
         # Plot = Controls if produces plots, defaults true (Boolean)
 
-        dat = self.dat
         shpdat = self.shapedat
         frames = self.frames 
         
@@ -146,7 +150,6 @@ class ttc(object):
         # Verbose: Provides additional info for debugging purposes (Boolean)
         # Plot = Controls if produces plots, defealts true (Boolean)
 
-        dat = self.dat
         frames = self.frames 
         shpdat = self.shapedat
         # Frames gives you the number of frames in the data file.
@@ -186,7 +189,6 @@ class ttc(object):
         # Verbose: gives additional information, assumed false. (Boolean)
         # Plot: whether or not it'll produce a plot
         
-        dat = self.dat
         frames = self.frames
         # Frames gives you the number of frames in the data file.
         shpdat = self.shapedat
@@ -211,6 +213,8 @@ class ttc(object):
         return avgdat
 
     def changemap(self,title = ' ',Verbose = False,Plot = True):
+        # Shows the net change in values over time from the first frame to the last
+        
         stuff = self.shapedat
         final = np.zeros((64,80))
         
@@ -263,6 +267,10 @@ class ttc(object):
         return shapelist
 
     def diffmap(self, factor, title = ' ',Verbose = False,Plot = True):
+        # Shows the net difference between the maximum and minumum values in each pixel,
+        #   but ignores the pixels below a certain threshold
+        #
+        # factor: controls the cutoff threshold, labelled as a factor times the average of the entire dataset
         stuff = self.shapedat
         thing1 = np.zeros((64,80))
         thing2 = np.zeros((64,80)) + 100000
@@ -283,6 +291,8 @@ class ttc(object):
         return raw
         
     def areaTry(self, factor, title = ' ',Verbose = False,Plot = True):
+        # Similar to diffmap, but smears what would be the final image to make areas 
+        #   of similar values more visually obvious
         stuff = self.shapedat
         thing1 = np.zeros((64,80))
         thing2 = np.zeros((64,80)) + 100000
@@ -313,9 +323,13 @@ class ttc(object):
         return raw
         
     def calib_max(self):   
+        # A function to retrieve calibration data from Toothpick Jig testing
+        #
+        # returns the mean of distances between high points in each frame, 
+        #   the standard deviation, and each frame with the relevant points marked
         maxfilter, maxima = np.copy(self.shapedat), np.copy(self.shapedat)
         distances, size = {}, 15
-        fuck = []
+        maxframes = []
         
         for i in range(len(self.shapedat)):
             maxfilter[i] = filters.maximum_filter(self.shapedat[i], size)
@@ -331,23 +345,99 @@ class ttc(object):
                 dis = (np.where(maxima[i] == True))
                 try:
                     distances[i] = ((dis[0][0]-dis[0][1])**2 + (dis[1][0]-dis[1][1])**2)**(0.5)
-                    fuck.append(maxima[i])
+                    maxframes.append(maxima[i])
                 except:
                     pass
         
         mean = np.mean(list(distances.values()))
         std = np.std(list(distances.values()))
         
-        return mean, std, fuck
+        return mean, std, maxframes
 
     def fix_leak(self, background):
+        # Accepts a recording of background data to eliminate current leak in data taken from camera
+        
         mask = background.avgmap(Plot = False) - np.amin(background.avgmap(Plot = False))
         
         if self.leakfix == False:
             new = self.shapedat - mask
-            self = ttc(new, leakfix = True)
+            fix = ttc(new, leakfix = True)
+            return fix
+        else:
+            return self
+    
+    def row_ani(self, line = "row 0", title=' ',time=200,terms=15):
+        # Creates an over-time animation of the line plot of a given row or column
+        # Defaults to the bottom row
+        #
+        # To save an animation, do 
+        # ani, plot_data = ttc.row_ani(line = "row/col #")
+        # ani.save('name.mp4')
         
-        return self
+        guide = {"row":80, "col":64}
+        
+        inpt = line.split()
+        if (((len(inpt) == 2) and (inpt[0] in guide)) and (type(inpt[1]==int))):
+            coord = inpt[0]
+            num = abs(int(inpt[1]))
+            if (num < guide[coord]):
+                pass
+            else:
+                print("This is not a valid entry.")
+                return False 
+        else:
+            print("This is not a valid entry.")
+            return False
+        
+        def fourier(x, *a):
+            ret = 0
+            for deg in range(len(a)):
+                ret += a[deg] * np.cos(((deg) * np.pi * x)/ guide[coord])
+            return ret
+        
+        if guide[coord] == 80:
+            data = [self.shapedat[:, num],[]]
+        elif guide[coord] == 64:
+            data = [self.shapedat[:, :, num],[]]
+
+        hold = [curve_fit(fourier, np.arange(guide[coord]), data[0][i], [1.0] * terms)[0] for i in range(guide[coord])]
+        data[1] = [[np.sum([(hold[k][i]*np.cos((i*np.pi*j)/guide[coord])) for i in range(terms)]) for j in range(guide[coord])] for k in range(len(hold))]
+            
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1,1,1)
+        ax1.set_ylim(np.amin(data[0])-1, np.amax(data[0])+1)
+        
+        ims = []
+        for i in range(guide[coord]):
+            im1, = ax1.plot(range(guide[coord]), data[0][i], color="black")
+            im2, = ax1.plot(range(guide[coord]), data[1][i], color="red")
+            ims.append([im1, im2])
+        
+        im_ani = animation.ArtistAnimation(fig, ims, interval=time, repeat_delay=1000,blit=True)
+
+        plt.show()
+        
+        return im_ani
+
+    def plot_smudge(self, num):
+        # Averages a number of adjacent frames to try to increase SNR
+        # Number of frames will match input ttc length by adding last frame as many times as necessary
+        #
+        # num: number of frames to be averaged
+        
+        if num > self.frames:
+            return False
+        
+        smudged = [(np.sum(self.shapedat[i: i+num], axis=0)/num) for i in range(self.frames-(num-1))]
+        
+        print(type(smudged))
+        for i in range(num-1):
+            smudged.append(smudged[-1])
+        
+        print(type(smudged))
+        return ttc(smudged)
+        
+        
 
 ######################################################################################### 
 ######################################################################################### 
